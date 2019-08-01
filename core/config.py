ab2daa2db24bdc476cc95d4bdbbb95d66850b59a
liuning-scu-cn <2742229056@qq.com>
2019-08-01 15:02:44 +0800

    add config.py

---
+++ core/config.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*
+
+# -------------------------------------------------------------------------------
+# Author: LiuNing
+# Contact: 2742229056@qq.com
+# Software: PyCharm
+# File: config.py
+# Time: 6/10/19 6:55 PM
+# Description: 
+# -------------------------------------------------------------------------------
+
+BATCH_SIZE = 10
+
+INPUT_SIZE = (288, 448)  # (h, w)
+RESIZE_SIZE = (324, 504)
+SAVE_FREQ = 10
+TEST_FREQ = 1
+RESUME = ''
+MIXUP = 0.
+
+SAVE_DIR = './model/'
+CUB_DIR = ''
+TOTAL_EPOCH = 30
+
+SEED = 666
