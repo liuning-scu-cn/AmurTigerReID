ac7d1fba14b2b11deaf7c05a4a00b06e07cd667e
liuning-scu-cn <2742229056@qq.com>
2019-08-01 15:03:16 +0800

    add seed.py

---
+++ core/seed.py
@@ -0,0 +1,13 @@
+import torch
+from core.config import SEED
+import numpy as np
+import random
+
+def init_environment():
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed(SEED)
+    torch.cuda.manual_seed_all(SEED)
+    np.random.seed(SEED)
+    random.seed(SEED)
+    torch.backends.cudnn.deterministic = True
+
